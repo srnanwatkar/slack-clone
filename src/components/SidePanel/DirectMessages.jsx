c4c8b73b7fdab3a289f7b779982821c2c2682fb5
Shantanu Nanwatkar <srnanwatkar@gmail.com>
2021-09-13 20:16:33 +0530

    added online offline logic

--- src/components/SidePanel/DirectMessages.jsx
+++ src/components/SidePanel/DirectMessages.jsx
@@ -5,10 +5,11 @@ import appFirebase from "../../firebase";
 
 function DirectMessages() {
 
+    const [users, handleUsers] = useState([]);
     const currentUser = useSelector(state => state.user_reducer.currentUser);
     const userRef = appFirebase.database().ref('users');
     const connectedRef = appFirebase.database().ref('.info/connected');
-    const [users, handleUsers] = useState([]);
+    const presenceRef = appFirebase.database().ref('presence');
 
     useEffect(() => {
         let loadedUsers = [];
@@ -25,9 +26,42 @@ function DirectMessages() {
 
     useEffect(() => {
         connectedRef.on('value', snap => {
-
+            if (snap.val() === true) {
+                const ref = presenceRef.child(currentUser.id);
+                ref.set(true);
+                ref.onDisconnect().remove(err => {
+                    if (err !== null) {
+                        console.log(err);
+                    }
+                });
+            }
         });
-    });
+
+        /* Child added trigger */
+        presenceRef.on('child_added', snap => {
+            if (currentUser.id !== snap.key) {
+                addStatusToUser(snap.key);
+            }
+        });
+        /* Child removed trigger */
+        presenceRef.on('child_removed', snap => {
+            if (currentUser.id !== snap.key) {
+                addStatusToUser(snap.key, false);
+            }
+        });
+    }, [JSON.stringify(users)]);
+
+    const addStatusToUser = (userId, connected = true) => {
+        const updatedUsers = users.reduce((acc, user) => {
+            if (user.uid === userId) {
+                user['status'] = `${connected ? 'online' : 'offline'}`
+            }
+            return acc.concat(user);
+        }, [])
+        handleUsers(updatedUsers)
+    }
+
+    const isUserOnline = (user) => user.status === 'online';
 
     return (
         <Menu.Menu className='menu'>
@@ -36,6 +70,14 @@ function DirectMessages() {
                     <Icon name='mail' /> DIRECT MESSAGES
                 </span>{' '}
             </Menu.Item>
+            {
+                users.map(user => (
+                    <Menu.Item key={user.uid} onClick={() => { console.log(user) }} style={{ opacity: '0.7', fontStyle: 'italic' }}>
+                        <Icon name='circle' color={isUserOnline(user) ? 'green' : 'red'} />
+                        @ {user.name}
+                    </Menu.Item>
+                ))
+            }
         </Menu.Menu>
     );
 }
